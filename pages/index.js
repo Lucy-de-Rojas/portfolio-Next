561b523dff497c252f56e6a18274f73fb810e228
lucyderojas <lucyderojas@gmail.com>
2022-01-21 12:41:58 +0000

    another

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,15 @@ export default function Home() {
 
 
       <h1>Lucy de Rojas Portfolio</h1>
+      <p>embding</p>
+
+      <embed src='/media/print/LondonEventsGuide2016.pdf' width="500px" height="300px" type="application/pdf" />
+
+      
+      <iframe src="https://onedrive.live.com/embed?resid=EC6BF2592866F3BD%21133&amp;authkey=%21AB0z1gC--kIE3K4&amp;em=2&amp;wdAr=1.7777777777777777&amp;Embed=1&amp;wdEaa=1" width="100%" height="691px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+
+
+
 
      
     </div>
